a99f97b4b983ba0f0feab5af8861e07cfbd3bf9e
Arfaqur <arfaqur@gmail.com>
2019-12-27 23:50:57 +0600

    Create A - Entrance Examination.cpp

---
+++ AtCoder Beginner Contest/ABC117/A - Entrance Examination.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    //cout<< static_cast<double>(a) / (double)(1.0 * b) <<'\n';
+    printf("%f\n", (float) a/b );
+    //printf("%lf\n", (double) a/b );
+    //cout<< (double)a / (double)b<<endl;
+    return 0;
+}
+
