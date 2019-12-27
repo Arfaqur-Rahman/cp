2f8c46b3e334c1e721f29ed19f12bcfff4103c0a
Arfaqur <arfaqur@gmail.com>
2019-12-27 15:34:20 +0600

    Create A - Double Helix.cpp

---
+++ AtCoder Beginner Contest/ABC122/A - Double Helix.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char c;
+    scanf("%c",&c);
+    switch(c){
+        case 'A': printf("T\n"); break;
+        case 'T': printf("A\n"); break;
+        case 'C': printf("G\n"); break;
+        case 'G': printf("C\n"); break;
+    }
+    return 0;
+}
+
+
+
