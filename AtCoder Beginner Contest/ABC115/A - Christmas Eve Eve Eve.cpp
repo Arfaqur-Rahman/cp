0725293358d9097eeed9eb16e5ccbc5013be6694
Arfaqur <arfaqur@gmail.com>
2020-01-02 23:36:45 +0600

    Update A - Christmas Eve Eve Eve.cpp

--- AtCoder Beginner Contest/ABC115/A - Christmas Eve Eve Eve.cpp
+++ AtCoder Beginner Contest/ABC115/A - Christmas Eve Eve Eve.cpp
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-#include<iostream>
-#include<stdio.h>
-using namespace std;
 
-int main(){
-    int d;
-    while(scanf("%d",&d)==1){
-        printf("Christmas");
-        while(d<25){
-            printf(" Eve");
-            d++;
-        }
-        puts("");
-    }
-    return 0;
-}
-=======
 #include<iostream>
 #include<stdio.h>
 
@@ -33,4 +16,4 @@ int main(){
     }
     return 0;
 }
->>>>>>> 135bc5baf7c5b486bbdd89c4b5526665f1aa80e4
+
