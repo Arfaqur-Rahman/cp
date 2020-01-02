192ce82abb45c9fd84c4a69d859db2125d8c63a4
Arfaqur <arfaqur@gmail.com>
2020-01-02 23:32:27 +0600

    Create A - Christmas Eve Eve Eve.cpp

---
+++ AtCoder Beginner Contest/ABC115/A - Christmas Eve Eve Eve.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+    int d;
+    while(scanf("%d",&d)==1){
+        printf("Christmas");
+        while(d<25){
+            printf(" Eve");
+            d++;
+        }
+        puts("");
+    }
+    return 0;
+}
