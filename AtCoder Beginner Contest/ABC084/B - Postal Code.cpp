9697ea1033a0f70b8e9a0b62ef0ec9d35280e283
Arfaqur-Rahman <arfaqur@gmail.com>
2020-02-04 01:55:32 +0600

    Update B - Postal Code.cpp

--- AtCoder Beginner Contest/ABC084/B - Postal Code.cpp
+++ AtCoder Beginner Contest/ABC084/B - Postal Code.cpp
@@ -10,7 +10,6 @@ main(void){
             return 0;
     }
     for(int i=0;i<a+b+1;i++){
-        if(s[a]==i) continue;
         if(s[i]=='0'|| s[i]=='1'|| s[i]=='2'|| s[i]=='3'|| s[i]=='4'|| s[i]=='5'|| s[i]=='6'|| s[i]=='7'|| s[i]=='8'|| s[i]=='9' ){
             c++;
         }
