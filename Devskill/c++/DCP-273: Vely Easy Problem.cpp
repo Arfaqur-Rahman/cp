c767cf9de3c25c637fe3dcc272fa467e5a0edad9
Arfaqur-Rahman <arfaqur@gmail.com>
2020-02-10 03:48:17 +0600

    Create DCP-273: Vely Easy Problem.cpp

---
+++ Devskill/c++/DCP-273: Vely Easy Problem.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    int n;
+    cin>>n;
+    while(n--){
+            string s;
+            cin>>s;
+            if(s.find("r") != string::npos) cout<<"Not vely easy"<<endl;
+           else cout<<"Vely easy"<<endl;
+        }
+}
+
