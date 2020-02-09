f4fe112ed6ed32805488b5650f27e47ebfed5508
Arfaqur-Rahman <arfaqur@gmail.com>
2020-02-09 03:13:01 -0700

    Create A - Red or Not.cs

---
+++ AtCoder Beginner Contest/ABC138/A - Red or Not.cs
@@ -0,0 +1,12 @@
+using System;
+class ProgramB
+    {
+        static void Main(string[] args)
+        {
+          	String a=Console.ReadLine();
+            int a1 = int.Parse(a); 
+            var s=Console.ReadLine();     
+            Console.WriteLine(((a1)<3200?"red": s));
+          
+        }
+    }
