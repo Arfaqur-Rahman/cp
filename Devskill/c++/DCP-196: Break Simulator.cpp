4f466d8fc9f8c3720cd3d913bf764d365a74fc52
Arfaqur-Rahman <arfaqur@gmail.com>
2020-02-10 03:56:16 +0600

    Create DCP-196: Break Simulator.cpp

---
+++ Devskill/c++/DCP-196: Break Simulator.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void){
+    int n;
+    cin>>n;
+    while(n--){
+            float v,t;
+            cin>>v>>t;
+            printf("%.2f\n",v/t*(-1));
+        }
+}
+
