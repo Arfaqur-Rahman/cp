5fc8aa807fb562a334e78bc5d9a51bb8fc0dec9f
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-03 14:33:46 +0600

    Create DCP-500: Wink or Kick.cpp

---
+++ Devskill/c++/DCP-500: Wink or Kick.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s;
+	while(cin>>s){
+   	if(s=="never"|| s=="be"|| s=="so"|| s=="happy"|| s=="when"|| s=="you"|| s=="are"|| s=="and"|| s=="sad") cout<<"YES"<< endl;
+   	else cout<<"NO"<< endl;
+   }
+   return 0;
+}
