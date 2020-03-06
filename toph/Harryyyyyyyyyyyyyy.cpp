0dac22381baf8bf9bc2de6d0e076737037b4f39f
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-06 12:34:13 +0600

    Create Harryyyyyyyyyyyyyy.cpp

---
+++ toph/Harryyyyyyyyyyyyyy.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	for(int i=1; i<=n;i++){
+        int t;
+        cin>>t;
+        cout<<"Case "<<i<<": Harr";
+        while(t--){
+            cout<<"y";
+        }
+		cout<<"! Khelbe Hogwarts, Jitbe Hogwarts!"<<endl;
+	}
+	return 0;
+}
