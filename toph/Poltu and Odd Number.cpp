8e3c0ea687edd0d950e089014eb94d32da9f23fc
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-07 08:10:56 +0600

    Update Poltu and Odd Number.cpp

--- toph/Poltu and Odd Number.cpp
+++ toph/Poltu and Odd Number.cpp
@@ -6,6 +6,7 @@ int main() {
   int t; sc("%d",&t); 
   for(int i=1;i<=t;i++){
     long long l,r; sc("%lld %lld",&l, &r); 
+    // if l and r aren't already odd, make them odd 
     if(l%2==0) l++;
     if(r%2==0) r--;
     // sum of n numbers in a mathematical series = n/2 * (2*a+ (n-1)*d ) where a=the starting number of the series and d= difference 
