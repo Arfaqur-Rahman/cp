7d72eb6bc3c6838bf2b21fac9c6e97f967042454
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-07 08:08:13 +0600

    Create Poltu and Odd Number.cpp

---
+++ toph/Poltu and Odd Number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+#define sc scanf
+int main() {
+  int t; sc("%d",&t); 
+  for(int i=1;i<=t;i++){
+    long long l,r; sc("%lld %lld",&l, &r); 
+    if(l%2==0) l++;
+    if(r%2==0) r--;
+    // sum of n numbers in a mathematical series = n/2 * (2*a+ (n-1)*d ) where a=the starting number of the series and d= difference 
+    long long n=((r-l)/2+1); 
+    printf("Case %d: %lld\n", i, (n* (2*l+ (n-1)*2 ) ) /2 );
+	}
+
+	return 0;
+}
+
+
