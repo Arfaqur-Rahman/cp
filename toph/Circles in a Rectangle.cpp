a9c70ada870a6d10702c3b1bc2a17a8aa35466cb
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-16 03:10:02 +0600

    Create Circles in a Rectangle.cpp

---
+++ toph/Circles in a Rectangle.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t; cin>>t; for(int i=1; i<=t; i++){float r; cin>>r; cout<<"Case "<<i<<": "<<fixed<<setprecision(2)<<(8- 2*3.1416)*r*r<<endl;}
+	return 0;
+}
