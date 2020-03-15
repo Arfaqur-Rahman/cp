199fc52fa9c532a45366727c7b2b88d52d87e3a1
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-15 23:05:40 +0600

    Create Jenia's Homework.cpp

---
+++ toph/Jenia's Homework.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t; cin>>t; while(t--){long double a; cin>>a; cout<<fixed<<setprecision(16)<< a - acos(-1)*a/4<<" "<<2*acos(-1)*sqrt(a)/2<<endl; }
+	return 0;
+}
