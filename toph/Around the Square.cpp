1c15b56bf54da02ab5fc053f89a0807dd9f95ce7
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-16 03:35:53 +0600

    Create Around the Square.cpp

---
+++ toph/Around the Square.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	double a, b, c, d, e;
+	while(cin>>a>>b>>c>>d>>e){cout<<fixed<<setprecision(3)<< a*a -  acos(-1)/4*( b*b + c*c + d*d + e*e ) <<endl;}
+	return 0;
+}
