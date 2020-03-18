ae5042b2a2eaaa8495f835c3ef647366283efa69
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-18 21:35:23 +0600

    Create Window Seats on a Bus.cpp

---
+++ toph/Window Seats on a Bus.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t, ca=0; cin>>t; while(t--){double l, w, d; cin>>l>>w>>d; cout<<fixed<<setprecision(2)<<"Case "<<++ca<<": "<<(w-d)/2*l <<endl;}
+	return 0;
+}
