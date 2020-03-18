12db3ddd9f86925e59886974e727e29846c13191
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-18 22:05:20 +0600

    Create Victory Robot.cpp

---
+++ toph/Victory Robot.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t; cin>>t; while(t--){int n; cin>>n; if(n==1971) cout<<"Joy Bangla\n"; else if(n & 1) cout<<"Joy\n"; else cout<<"Bangla\n"; }
+	return 0;
+}
