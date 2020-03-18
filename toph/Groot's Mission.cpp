08aaa22eb937779241057a3aeafc25479b86879b
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-19 04:57:47 +0600

    Create Groot's Mission.cpp

---
+++ toph/Groot's Mission.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t; cin>>t; while(t--){ int n; cin>>n; for(int i=0; i<n; i++){ int k; cin>>k; } cout<<n-1<<endl; }
+	return 0;
+}
