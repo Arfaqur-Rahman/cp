dda6b6a75f1042d3201c67ab79591af4a99bd0f8
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-19 03:16:46 +0600

    Create Say it !.cpp

---
+++ toph/Say it !.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t; cin>>t; while(t--){char c; cin>>c; cout<< ( islower(c) ? "lowercase" : "UPPERCASE" ) <<"\n"; }
+	return 0;
+}
