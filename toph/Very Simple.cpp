f0f7e762769ec246be2f229077d81bd58773986b
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-19 15:53:17 +0600

    Create Very Simple.cpp

---
+++ toph/Very Simple.cpp
@@ -0,0 +1,9 @@
+//  you are given a number N. Determine what is N’th number which has odd number of divisor.
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	long long t; cin>>t; while(t--){ long long n; cin>>n; cout<<n*n<<endl;}
+	return 0;
+}
