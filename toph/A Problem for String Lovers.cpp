e0ce727be03ff5a2953e6109f5f2f7c5bd2ff3ae
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-27 07:13:38 +0600

    Create A Problem for String Lovers.cpp

---
+++ toph/A Problem for String Lovers.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t; cin>>t; getchar(); while(t--){string s; getline(cin, s); reverse(s.begin(), s.end()); cout<<s<<endl;}
+	return 0;
+}
