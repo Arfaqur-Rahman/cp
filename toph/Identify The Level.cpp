2297c2a72f558ad6642449ecbbc6c9f0160cbb0c
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-27 09:55:05 +0600

    Update Identify The Level.cpp

--- toph/Identify The Level.cpp
+++ toph/Identify The Level.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main() {
 	int t; cin>>t;
 	for(int j=1; j<=t; j++){
-		int n, i, s=0;
-		cin>>n;
+	  int n;
+	  cin>>n;
 	  int k=log2(n);
-		cout<<"Case "<<j<<": "<<k+1<<endl;
+	  cout<<"Case "<<j<<": "<<k+1<<endl;
 	}
 	return 0;
 }
