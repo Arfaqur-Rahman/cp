784d63588d2e7043c04a4585e29e48b20dae07ed
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-27 09:50:29 +0600

    Create Identify The Level.cpp

---
+++ toph/Identify The Level.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	for(int j=1; j<=t; j++){
+		int n, i, s=0;
+		cin>>n;
+	  int k=log2(n);
+		cout<<"Case "<<j<<": "<<k+1<<endl;
+	}
+	return 0;
+}
