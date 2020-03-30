7c2f14c2287826577af4d8585a73fc85d9f7d431
Arfaqur-Rahman <arfaqur@gmail.com>
2020-03-30 10:16:13 +0600

    Create Zacob The Batsman.cpp

---
+++ toph/Zacob The Batsman.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+        long long c;
+        cin>>c;
+        cout<<( (c%2==0 && c>3) || (C==0) ? "Zacob Zacob!": "Sorry Zacob!")<<endl;
+	}
+	return 0;
+}
+
+
+
+
