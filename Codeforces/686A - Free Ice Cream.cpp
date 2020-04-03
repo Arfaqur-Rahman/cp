97fcb43a3acb48e238cd3eccd2da000f5060bd23
Arfaqur-Rahman <arfaqur@gmail.com>
2020-04-03 08:03:18 +0600

    Create 686A - Free Ice Cream.cpp

---
+++ Codeforces/686A - Free Ice Cream.cpp
@@ -0,0 +1,20 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+
+int main(){
+    int n;
+    long long m;
+    cin>>n>>m;
+    int dis=0;
+    while(n--){
+      char c;
+      int k;
+      cin>>c>>k;
+      if(c=='-'){
+          if(m-k>=0) m-=k;
+          else dis++;
+      }
+      else m+=k;
+    }
+    cout<<m<<" "<<dis;
+ }
