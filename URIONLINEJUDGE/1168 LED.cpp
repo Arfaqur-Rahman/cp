a87011a75eeb7298ab322aa90a2dc6002ad0f619
Arfaqur-Rahman <arfaqur@gmail.com>
2020-04-07 10:46:01 +0600

    Create 1168 LED.cpp

---
+++ URIONLINEJUDGE/1168 LED.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int numOfLeds(char c){
+    switch(c){
+    case '1':
+        return 2;
+    case '2':
+        return 5;
+    case '3':
+        return 5;
+    case '4':
+        return 4;
+    case '5':
+        return 5;
+    case '6':
+        return 6;
+    case '7':
+        return 3;
+    case '8':
+        return 7;
+    case '9':
+        return 6;
+    default:
+        return 6;
+    }
+}
+
+
+int main() {
+    int t; cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        long long sum=0;
+        for(int i=0; s[i]; i++) sum+= numOfLeds(s[i]);
+        cout<<sum<<" leds"<<endl;
+    }
+    return 0;
+}
+
