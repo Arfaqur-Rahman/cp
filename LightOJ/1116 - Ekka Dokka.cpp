3b4cdf575059b8927f5585c48a4b19577fc11ea5
Arfaqur-Rahman <arfaqur@gmail.com>
2020-05-04 16:39:16 +0600

    Create 1116 - Ekka Dokka.cpp

---
+++ LightOJ/1116 - Ekka Dokka.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int t;
+    cin>>t;// 001=4  101=5
+    for(int i=1; i<=t; i++){
+        long long n;
+        cin>>n;
+        cout<<"Case "<<i<<": ";
+        if(n & 1){
+            cout<<"Impossible"<<endl;
+            continue;
+        }
+        for(long long j=2; j<=n; j*=2 ){
+            long long odd= n/j;
+            if(odd & 1){
+                cout<<odd <<" "<<j<<endl;
+                break;
+            }
+        }
+    }
+}
+
+
