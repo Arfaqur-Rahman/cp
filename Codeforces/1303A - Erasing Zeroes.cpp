4648a23700f54ccbb6e7b707595ea9f84b85528a
Arfaqur-Rahman <arfaqur@gmail.com>
2020-05-07 08:28:29 +0600

    Update 1303A - Erasing Zeroes.cpp

--- Codeforces/1303A - Erasing Zeroes.cpp
+++ Codeforces/1303A - Erasing Zeroes.cpp
@@ -29,3 +29,35 @@ int main(){
     }
 
 }
+
+
+
+/* Another
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        string s;
+        cin>>s;
+        int starting1=-1;
+        for(int i=0; s[i]; i++){
+            if(s[i]=='1'){starting1=i; break;}
+        }
+
+        int ending1=-1;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s[i]=='1'){ending1=i; break;}
+        }
+
+        int numof0=0;
+        for(int i=starting1; i<=ending1; i++){
+            if(s[i]=='0') numof0++;
+        }
+        cout<<numof0<<endl;
+    }
+
+}
+*/
