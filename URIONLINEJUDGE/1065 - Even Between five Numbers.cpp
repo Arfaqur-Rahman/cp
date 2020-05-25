0bc6a453e06e155a3875bcdd4bf84b674fcb595c
Arfaqur-Rahman <arfaqur@gmail.com>
2020-05-26 01:33:29 +0600

    Create  1065 - Even Between five Numbers.cpp

---
+++ URIONLINEJUDGE/1065 - Even Between five Numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     int n, c=0;
+     while(cin>>n){
+         if( n % 2==0 ) c++;
+     } 
+     cout<<c<<" valores pares"<<endl;
+    return 0;
+}
