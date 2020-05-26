c4544b2e7a4c6afa55016aaf33d8f95c73dec36a
Arfaqur-Rahman <arfaqur@gmail.com>
2020-05-26 14:20:26 +0600

    Create 1178 - Array Fill III.cpp

---
+++ URIONLINEJUDGE/1178 - Array Fill III.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    double a[100];
+    double n;
+    cin>>n;
+    
+    for(int i=0; i<100; i++){
+        cout<<fixed<<setprecision(4)<<"N["<<i<<"] = "<<n<<endl;
+        n/=2;
+    }
+    return 0;
+}
