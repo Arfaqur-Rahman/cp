0e20b41dabae30fea07146ab49c8fe0ba79d2682
Arfaqur-Rahman <arfaqur@gmail.com>
2020-05-30 19:30:17 +0600

    Create 272 - TEX Quotes.cpp

---
+++ UVA/272 - TEX Quotes.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    string s;
+    bool b= false;
+    while( getline(cin,s)){
+         for(int i=0; s[i]; i++){
+            if( !b && s[i]=='"'){
+                cout<<"``";
+                b=true;
+            }
+            else if( b && s[i]=='"'){
+                cout<<"''";
+                b=false;
+            }
+            else cout<<s[i];
+        }
+        cout<<endl;
+    }
+   
+}
