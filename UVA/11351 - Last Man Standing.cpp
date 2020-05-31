bfa9a7511b262851ab77b598edce7ddbd38f1b3c
Arfaqur-Rahman <arfaqur@gmail.com>
2020-05-31 13:15:01 +0600

    Create 11351 - Last Man Standing.cpp

---
+++ UVA/11351 - Last Man Standing.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+int josephus(int n, int k){
+    if (n==1) return 1;
+    else return (josephus(n-1, k) + k - 1)% n + 1;
+}
+
+
+signed main(){
+    int t;
+    cin>>t;
+    for(int i=1; i<=t; i++){
+        int n, k;
+        cin>>n>>k;
+        cout<<"Case "<<i<<": "<<josephus(n, k)<<endl;
+
+    }
+}
