6c6ac4e0da5f4637bffa0acf7544c3c780c6402f
Arfaqur-Rahman <arfaqur@gmail.com>
2020-06-23 00:29:24 +0600

    Update 10127 - Ones.cpp

--- UVA/10127 - Ones.cpp
+++ UVA/10127 - Ones.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #define sccc(n1,n2,n3) scanf("%lld %lld %lld",&n1,&n2,&n3)
 #define debug(x) cerr<<#x<<": "<<x<<endl;
 
-
+// https://www.mathblog.dk/uva-10127-ones/
 signed main() {
     FAST;
     int n;
@@ -18,7 +18,7 @@ signed main() {
         int c=1;
         while(1){
             if(sum%n==0 ) break;
-            sum = (sum*10 + 1)%n;
+            sum = (sum*10 + 1)%n; // https://codingstrife.wordpress.com/2013/07/28/solution-uva10127-pc110504-ones/
             c++;
         }
         cout<<c<<endl;
