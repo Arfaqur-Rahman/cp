a98fda5bd1d7d70b4140474707da8d9c5efaf5ea
Arfaqur-Rahman <arfaqur@gmail.com>
2020-07-02 20:09:17 +0600

    Create 543 - Goldbach's Conjecture.cpp

---
+++ UVA/543 - Goldbach's Conjecture.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1000000;
+bool isPrime[N + 1];
+void sieve() { // O(N lg lg N)
+   for(int i = 0; i <= N; i++) {
+       isPrime[i] = true;
+   }
+   isPrime[0] = isPrime[1] = false;
+   for(int i = 2; i * i <= N; i++) {
+       if(isPrime[i] == true) {
+           for(int j = i * i; j <= N; j += i) {
+               isPrime[j] = false;
+           }
+       }
+   }
+}
+
+
+
+
+int main(){
+    sieve();
+    
+    int n;
+    while(cin>>n && n){
+        for(int i=2; i<=n; i++){
+            if(isPrime[i]==true && isPrime[n-i]==true){cout<<n<<" = "<<i<<" + "<<n-i<<endl; break;}
+        }
+    }
+}
