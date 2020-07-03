e648f93c37432fa79b1d140a3124a9381066ebbe
Arfaqur-Rahman <arfaqur@gmail.com>
2020-07-03 17:43:07 +0600

    Update 10924 - Prime Words.cpp

--- UVA/10924 - Prime Words.cpp
+++ UVA/10924 - Prime Words.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 bool isPrime(int n) {
-   if(n == 1) return true;
+   if(n == 1) return true; //In this problem 1 is considered prime  
    if(n == 2) return true;
    for(int i = 2; i * i <= n; i++) {
        if( n % i == 0) return false;
