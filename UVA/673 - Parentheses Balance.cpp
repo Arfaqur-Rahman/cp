623e6f070275d3c07a682bb367743ba94b587880
Arfaqur-Rahman <arfaqur@gmail.com>
2020-07-06 09:04:27 +0600

    Create 673 - Parentheses Balance.cpp

---
+++ UVA/673 - Parentheses Balance.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+bool stackCheck(string& s){
+    stack<char> st;
+    for(int i=0; s[i]; i++){
+        if(s[i]=='(' || s[i]=='[') st.push(s[i]);
+        else{
+            if(st.empty()) return false;
+            else if(s[i]==')' && st.top()=='[') return false;
+            else if(s[i]==']' && st.top()=='(') return false;
+            else st.pop();
+        }
+    }
+    return st.empty();
+}
+
+signed main(){
+    int t;
+    cin>>t;
+    cin.ignore();
+    while(t--){
+        string s;
+        getline(cin, s);
+        cout<< ( stackCheck(s) ? "Yes" : "No" ) <<endl;
+    }
+}
