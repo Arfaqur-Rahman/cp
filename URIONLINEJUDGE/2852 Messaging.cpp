327611769661908019477e2e0da6ea31d0ebcfe1
Arfaqur-Rahman <arfaqur@gmail.com>
2020-07-20 02:45:32 +0600

    Create 2852 Messaging.cpp

---
+++ URIONLINEJUDGE/2852 Messaging.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string k;
+    cin>>k;
+
+    int t;
+    scanf("%d", &t); getchar();
+    while(t--){
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        string x;
+        int i=0; //while(cin>>x){
+        while(ss>>x){
+            if(x[0]=='a' || x[0]=='e' || x[0]=='i' || x[0]=='o' || x[0]=='u') cout<<x;
+            else{
+
+                for(char each: x){
+                   int temp= each + (k[i%k.length()] -'a');
+                   if(temp>'z') cout<< (char)('a' + (temp%'z' ) -1 );
+                   else cout<<(char)(temp);
+                    i++;
+                }
+             
+            }
+            if ( !(ss.eof())  ) cout<<" "; // if this is not checked, presentation error is encountered!
+        }
+        cout<<'\n';
+    }
+}
