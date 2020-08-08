10442e331af0c27e7735ad8fbf6c0d1695b0f2d0
Arfaqur-Rahman <arfaqur@gmail.com>
2020-08-08 08:59:06 +0600

    Create 7. Word.cpp

---
+++ A2OJ Ladders/Codeforces Rating < 1300/7. Word.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s; cin>>s;
+    int u=0, l=0;
+    for(int i=0;s[i]!='\0';i++){
+        if(isupper(s[i])){u++;}
+        else{l++;}
+    }
+    if(u>l){
+        for(int i=0;s[i]!='\0';i++){
+           s[i]=toupper(s[i]);
+        }
+        cout<<s;
+    }
+    else{
+        for(int i=0;s[i]!='\0';i++){
+           s[i]=tolower(s[i]);
+        }
+        cout<<s;
+    }
+}
