f51c46f8cbaa71c9b9a65e851a1d81888c8b2a64
Arfaqur-Rahman <arfaqur@gmail.com>
2020-08-30 21:54:43 +0600

    Create 11614	Etruscan Warriors Never Play Chess.cpp

---
+++ UVA/11614	Etruscan Warriors Never Play Chess.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+int main(){
+
+    int tc;
+    scanf("%d", &tc);
+    while(tc--){
+        ll n;
+        scanf("%lld", &n);
+        ll r = ( -1 + sqrt(1+8*n))/2;
+        printf("%lld\n", r );
+
+   }
+}
