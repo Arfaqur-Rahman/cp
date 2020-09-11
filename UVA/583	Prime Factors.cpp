66916845c3dd607f62177c0baff2491c067e4d82
Arfaqur-Rahman <arfaqur@gmail.com>
2020-09-11 18:09:59 +0600

    Create 583	Prime Factors.cpp

---
+++ UVA/583	Prime Factors.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int N = (int)4e7;
+bool pr[N + 1];
+vector<int> v;
+
+vector<ll> base, power;
+
+void sieve() {
+	for(int i = 0; i <= N; i++) {
+		pr[i] = true;
+	}
+
+	pr[0] = pr[1] = false;
+
+	for(int i = 2; i * i <= N; i++ ) {
+		if(pr[i]) {
+			for(int j = i * i; j <= N; j += i) {
+				pr[j] = false;
+			}
+		}
+	}
+
+	for(int i = 2; i <= N; i++) {
+		if(pr[i]) {
+			v.push_back(i);
+		}
+	}
+
+}
+
+
+
+
+void primeFactorize(long long n) {
+
+    for(int i = 0; i < v.size(); i++) {
+    	int p = v[i];
+
+    	if((long long) p * p > n) break;
+
+        if( n % p == 0) {
+            int cnt = 0;
+            while (n % p == 0) {
+                n /= p;
+                cnt++;
+            }
+
+            base.push_back(p);
+            power.push_back(cnt);
+
+        }
+    }
+
+    if(n > 1){
+    	base.push_back(n);
+        power.push_back(1);
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    sieve();
+
+    int n;
+    while(cin>>n && n){
+        bool b= n<0 ;
+        cout<<n<<" = ";
+        if(b) cout<<"-1";
+
+        primeFactorize(abs(n));
+
+        bool f=true;
+		for(int i=0; i<base.size(); i++) {
+
+            int temp=power[i];
+            while(temp--){
+                if(!b) {cout<<base[i]; b=!b;}
+                else cout<<" x "<<base[i];
+
+            }
+		}
+        base.clear();
+        power.clear();
+        cout<<endl;
+    }
+}
