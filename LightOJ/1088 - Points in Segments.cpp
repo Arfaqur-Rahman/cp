6589520823151d6657024be543570c83589f8f00
Arfaqur-Rahman <arfaqur@gmail.com>
2020-09-25 17:02:03 +0600

    Create 1088 - Points in Segments.cpp

---
+++ LightOJ/1088 - Points in Segments.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+    int tc;
+    scanf("%d",&tc);
+    for(int t=1; t<=tc; t++){
+        int n, q;
+        scanf("%d %d", &n, &q);
+        vector<int> v(n);
+        for(int& x: v) scanf("%d",&x);
+
+        printf("Case %d:\n", t);
+
+        int it1, it2;
+        while(q--){
+            int a, b;
+            scanf("%d %d", &a, &b);
+            it1=lower_bound(v.begin(), v.end(), a)-v.begin();
+            it2=upper_bound(v.begin(), v.end(), b)-v.begin();
+            printf("%d\n", it2-it1);
+
+        }
+
+    }
+}
