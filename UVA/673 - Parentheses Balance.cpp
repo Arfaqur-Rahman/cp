97180b70008c90654adc375832f8b88df7fdeb95
Arfaqur-Rahman <arfaqur@gmail.com>
2020-12-28 01:45:48 +0600

    Update 673 - Parentheses Balance.cpp

--- UVA/673 - Parentheses Balance.cpp
+++ UVA/673 - Parentheses Balance.cpp
@@ -1,28 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long
 
-bool stackCheck(string& s){
-    stack<char> st;
-    for(int i=0; s[i]; i++){
-        if(s[i]=='(' || s[i]=='[') st.push(s[i]);
-        else{
-            if(st.empty()) return false;
-            else if(s[i]==')' && st.top()=='[') return false;
-            else if(s[i]==']' && st.top()=='(') return false;
-            else st.pop();
-        }
-    }
-    return st.empty();
-}
+#define ll long long
+#define endl '\n'
 
-signed main(){
-    int t;
-    cin>>t;
-    cin.ignore();
+int main(){
+
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+    int t; cin>>t; cin.ignore();
     while(t--){
-        string s;
-        getline(cin, s); // WA is encountered if getline is not used
-        cout<< ( stackCheck(s) ? "Yes" : "No" ) <<endl;
+        string s; getline(cin, s);
+
+        stack<char> st;
+   
+        for (int i = 0; s[i]; i++) {
+            if (s[i] == ')') {
+                if (!st.empty() && st.top() == '(') st.pop();
+                else st.push(s[i]);
+            } else if (s[i] == ']') {
+                if (!st.empty() && st.top() == '[') st.pop();
+                else st.push(s[i]);
+            } else st.push(s[i]);
+        }
+        if(st.empty()) cout<<"Yes"<<endl; else cout<<"No\n";
+        
+
     }
 }
