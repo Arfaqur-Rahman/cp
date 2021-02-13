f6d03d7a45d3df443a24ad57667466550d33b0cc
Arfaqur-Rahman <arfaqur@gmail.com>
2021-02-13 17:35:08 +0600

    Update 10812 - Beat the Spread!.cpp

--- UVA/10812 - Beat the Spread!.cpp
+++ UVA/10812 - Beat the Spread!.cpp
@@ -1,22 +1,38 @@
+
 #include<bits/stdc++.h>
 using namespace std;
-
 #define ll long long
-int main() {
+#define endl '\n'
 
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-
-    int t; cin >> t;
-    while (t--) {
-    
-        ll s, d; cin >> s >> d;
-
-        ll a = (s + d) / 2;
-
-        ll b = abs(s - d) / 2;
-
-        if (a + b == s && (a - b == d || b - a == d) ) {if (a > b)cout << a << " " << b << endl; else cout << b << " " << a << endl;}
-        else cout << "impossible\n";
-        
+    int t; cin>>t;
+    while(t-- ){
+        ll s,d; cin>>s>>d;
+        if(s<d) cout<<"impossible\n";
+        else {
+            ll a=(s+d)/2;
+            ll  b=s-a;
+            ll ma=max(a, b);
+            ll mb=min(a, b);
+            if(ma+mb==s && ma-mb==d )cout<<ma<<" "<<mb<<endl;
+            else cout<<"impossible\n";
+        }
     }
+
 }
+
+/*
+4
+0 0
+0 1
+1 0
+1 1
+
+
+0 0
+impossible
+impossible
+1 0
+*/
