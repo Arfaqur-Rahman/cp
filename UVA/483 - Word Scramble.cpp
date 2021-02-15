76f0ce9e78976810c3b27e2e852cfcd8fa3bcf86
Arfaqur-Rahman <arfaqur@gmail.com>
2021-02-15 09:37:45 +0600

    Update 483 - Word Scramble.cpp

--- UVA/483 - Word Scramble.cpp
+++ UVA/483 - Word Scramble.cpp
@@ -12,3 +12,39 @@ int main(){
 //links to other sweet solutions of this same problem: 
 //https://cainiaozr.wordpress.com/2012/11/09/uva-oj-483-word-scramble/
 //https://www.cnblogs.com/TheLaughingMan/archive/2013/02/24/2926728.html
+
+/* Another
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    string s;
+    while(getline(cin, s)){
+        string temp;
+        for(int i=0; s[i]; i++){
+            if(s[i]!=' '){
+                temp+=s[i];
+            }else{
+                reverse(temp.begin(), temp.end());
+                cout<<temp<<" ";
+                temp.clear();
+            }
+        }
+        reverse(temp.begin(), temp.end());
+        cout<<temp<<endl;
+    }
+
+
+}
+
+
+
+/*
